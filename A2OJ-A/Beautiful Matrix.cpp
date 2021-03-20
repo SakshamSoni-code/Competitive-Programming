dad6f99983402cca273d4f93ee36ea7a2b0edc2d
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2021-03-20 11:28:15 +0530

    Beautiful Matrix-A

---
+++ A2OJ-A/Beautiful Matrix.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int a;
+    
+    for(int i=1;i<6;i++)
+    {
+        for(int j=1;j<6;j++)
+        {
+            cin>>a;
+            if(a==1)
+            {
+             cout<<(abs(3-i)+abs(3-j))<<endl;
+            }
+        }
+    }
+    return 0;
+}
