a97652f99d3514dfaf55e830c040c0745e695c5c
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2021-09-21 13:38:11 +0530

    Add files via upload

---
+++ CODEFORCES DIV2/Educational Round 114 (20-09-21)/1574-A.cpp
@@ -0,0 +1,79 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+#define check       cout<<checkedDude<<endl
+
+const ll mod = 1000000007; //10^9+7
+
+using namespace std;
+
+/*
+int gcdx( ll a, ll b)
+         {
+          if(b==0)return a%b;
+          else return gcdx(b,a%b);
+         }
+*/
+void problem_solver()
+{
+    int n;
+    cin>>n;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++) cout<<"(";
+        for(int j=1;j<=i;j++) cout<<")";
+        for(int j=i+1;j<=n;j++) cout<<"()";
+        cout<<endl;
+    }
+}
+
+int32_t main() {
+    
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        problem_solver();
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
---
+++ CODEFORCES DIV2/Educational Round 114 (20-09-21)/1574-B.cpp
@@ -0,0 +1,78 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+#define check       cout<<checkedDude<<endl
+
+const ll mod = 1000000007; //10^9+7
+
+using namespace std;
+
+/*
+int gcdx( ll a, ll b)
+         {
+          if(b==0)return a%b;
+          else return gcdx(b,a%b);
+         }
+*/
+void problem_solver()
+{
+    ll a,b,c,m,max;
+    cin>>a>>b>>c>>m;
+    
+    if((a-1)+(b-1)+(c-1)<m)cout<<"NO"<<endl;
+    else{
+        if((a-1)>b+c+m || (b-1)>a+c+m || (c-1)>a+b+m) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+}
+
+int32_t main() {
+    
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        problem_solver();
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
