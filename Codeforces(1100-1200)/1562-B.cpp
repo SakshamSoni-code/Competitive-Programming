929d1c46ef485143e8ba6fe42079d604ca0d2eaf
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2021-09-24 14:49:41 +0530

    Add files via upload

---
+++ Codeforces(1100-1200)/1562-B.cpp
@@ -0,0 +1,96 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+#define check       cout<<checkedDude<<endl
+
+const ll mod = 1000000007; //10^9+7
+
+using namespace std;
+
+/*
+int gcdx( ll a, ll b)
+         {
+          if(b==0)return a%b;
+          else return gcdx(b,a%b);
+         }
+*/
+void problem_solver()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    
+    for(int i=0;i<n;i++){
+        if(s[i]=='1' || s[i]=='4' || s[i]=='6' || s[i]=='8' || s[i]=='9'){
+            cout<<1<<endl;
+            cout<<s[i]<<endl;
+            return;
+        }
+    }
+    
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+          int temp=(s[i] - '0') * 10 + (s[j] - '0');
+          
+          for(int i=2;i<temp;i++){
+              if(temp%i==0){
+                  cout<<2<<endl;
+                  cout<<temp<<endl;
+                  return;
+              }
+          }
+        }
+    }
+}
+
+int32_t main() {
+    
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        problem_solver();
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
