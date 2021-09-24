ae6d5e8880a1f25909ef6c8535d492de5cc1d90c
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2021-09-24 12:42:27 +0530

    Add files via upload

---
+++ Codeforces(1100-1200)/1476-A.cpp
@@ -0,0 +1,88 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+#define check       cout<<checkedDude<<endl
+
+const ll mod = 1000000007; //10^9+7
+
+using namespace std;
+
+/*
+int gcdx( ll a, ll b)
+         {
+          if(b==0)return a%b;
+          else return gcdx(b,a%b);
+         }
+*/
+void problem_solver()
+{
+    ll n,k;
+    cin>>n>>k;
+    
+    if(n==1) {
+        cout<<k<<endl;
+        return;
+    }
+    else if(n==k) {
+        cout<<1<<endl;
+        return ;
+    }
+    
+    else if(n>k){
+        k= ((n+k-1)/k)*k;
+    }
+
+    cout<<(n+k-1)/n<<endl;
+    
+}
+
+int32_t main() {
+    
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        problem_solver();
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
